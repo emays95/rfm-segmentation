7552bde465b1e0a7ab6898ad456a6423d71aac17
emays95 <emays95.dev@gmail.com>
2025-08-28 11:43:37 -0700

    oltp model updated, etl from csv to oltp notebook

--- sql/oltp/oltp_table.sql
+++ sql/oltp/oltp_table.sql
@@ -1,33 +1,40 @@
 CREATE TABLE IF NOT EXISTS trans(
     trans_id VARCHAR(16) PRIMARY KEY,
     trans_date DATE,
-    customer_name VARCHAR(64),
     total_cost DECIMAL,
     payment_method VARCHAR(32),
     city VARCHAR(32),
     store_type VARCHAR(32),
     discount_applied BOOLEAN,
-    promotion VARCHAR(64)
+    promotion VARCHAR(64),
+    cust_id VARCHAR(16)
+);
 
-)
 
-
-CREATE IF NOT EXISTS trans_prod(
-    trans_id VARCHAR(16),
+CREATE TABLE IF NOT EXISTS trans_prod(
     product VARCHAR(32),
-    PRIMARY KEY (trans_id, product)
-)
-
-CREATE SEQUENCE cust_ID_seq
-    START WITH 1
-    INCREMENT BY 1
-    MINVALUE 1
-    NO MAXVALUE
-    OWNED BY cust.cust_id
+    trans_id VARCHAR(16),
+	product_id VARCHAR(16),
+    PRIMARY KEY (trans_id, product_id)
+);
 
 
-CREATE IF NOT EXISTS cust(
-    cust_id INT DEFAULT nextval('cust_id_seq') PRIMARY KEY,
+-- CREATE SEQUENCE cust_ID_seq
+--     START WITH 1
+--     INCREMENT BY 1
+--     MINVALUE 1
+--     NO MAXVALUE
+--     OWNED BY cust.cust_id
+
+
+CREATE TABLE IF NOT EXISTS cust(
+--     cust_id INT DEFAULT nextval('cust_id_seq') PRIMARY KEY,
     customer_name VARCHAR(64),
-    customer_category VARCHAR(32)
+    customer_category VARCHAR(32),
+    cust_id VARCHAR(16) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS product(
+    product VARCHAR(32),
+    product_id VARCHAR(16) PRIMARY KEY
 );
